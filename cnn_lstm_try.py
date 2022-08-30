3ea43dfc3cf3f7b0a9c61c05fe6fb72923a6dc73
arghasen10 <arghasen10@gmail.com>
2022-08-31 05:11:32 +0530

    updated py files

--- cnn_lstm_try.py
+++ cnn_lstm_try.py
@@ -1,318 +1,207 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 from library.helper import load_dataset
-
-
-# In[2]:
-
-
-df=load_dataset()
-
-
-# In[3]:
-
-
-class_map={
- 'looking forward': 0,
- 'looking right': 1,
- 'looking left': 2,
- 'yawning': 3,
- 'looking up': 4,
- 'Talking': 5}
-
-df['activity']=df.activity.map(class_map)
-
-
-# In[4]:
-
-
-features=['doppz']
-label='activity'
-
-
-# In[5]:
-
-
 from sklearn.ensemble import RandomForestClassifier
-
-
-# In[6]:
-
-
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import f1_score,classification_report
-
-
-# In[7]:
-
-
-kf=KFold(n_splits=3,shuffle=True,random_state=42)
-
-
-# In[8]:
-
-
-extract_flat_feat=lambda ddf:np.array(ddf[features].values.tolist())[:,:,:,:9].reshape(-1,128*9)
-
-
-# In[9]:
-
-
-def evaluate(model_fn,process_fn=None):
-    train_f1=[]
-    test_f1=[]
-    for train_idx,test_idx in kf.split(df):
-        train_df=df.iloc[train_idx].copy()
-        test_df=df.iloc[test_idx].copy()
-
-        X_train=process_fn(train_df)
-        y_train=train_df['activity']
-
-        X_test=process_fn(test_df)
-        y_test=test_df['activity']
-
-        scaler=MinMaxScaler()
-        X_train=scaler.fit_transform(X_train)
-        X_test=scaler.transform(X_test)
-        
-        model=model_fn()
-        model.fit(X_train,y_train)
-
-        pred=model.predict(X_train)
-        #print(classification_report(y_train,pred))
-        train_f1.append(f1_score(y_train,pred,average='weighted'))
-
-        pred=model.predict(X_test)
-        #print(classification_report(y_test,pred))
-        test_f1.append(f1_score(y_test,pred,average='weighted'))
-
-    plt.boxplot(x=[train_f1,test_f1],positions=[0,1])
-    plt.show()
-
-
-# In[10]:
-
-
-rf_model_fn=lambda: RandomForestClassifier(n_estimators=30,criterion='gini',max_depth=20)
-evaluate(rf_model_fn,extract_flat_feat)
-
-
-# In[11]:
-
-
-#CNN
-
-
-# In[10]:
-
-
+from sklearn.metrics import f1_score, classification_report
 import tensorflow as tf
-
-
-# In[10]:
 
 
 class My2dCNN:
     def __init__(self):
-        self.history=None
-        self.model=self.build_network()
+        self.history = None
+        self.model = self.build_network()
         self.model.summary()
-    
+
     def build_network(self):
-        cnn=tf.keras.Sequential([
-            tf.keras.layers.Conv2D(64, (8,9), (3,1), activation='relu', padding="valid", input_shape=(128,9,1)),
-            tf.keras.layers.Conv2D(96, (5,1), (3,1), activation='relu', padding="valid"),
-            tf.keras.layers.Conv2D(128, (5,1), (2,1), activation='relu', padding="valid"),
+        cnn = tf.keras.Sequential([
+            tf.keras.layers.Conv2D(64, (8, 9), (3, 1), activation='relu', padding="valid", input_shape=(128, 9, 1)),
+            tf.keras.layers.Conv2D(96, (5, 1), (3, 1), activation='relu', padding="valid"),
+            tf.keras.layers.Conv2D(128, (5, 1), (2, 1), activation='relu', padding="valid"),
             tf.keras.layers.GlobalAvgPool2D(),
-            tf.keras.layers.Dense(64,activation='relu'),
-            tf.keras.layers.Dense(6,activation='softmax')
-        ],name='2dCNN')
-        cnn.compile(loss='sparse_categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+            tf.keras.layers.Dense(64, activation='relu'),
+            tf.keras.layers.Dense(6, activation='softmax')
+        ], name='2dCNN')
+        cnn.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
         return cnn
-    
-    def fit(self,X,y,epoch=400,batchsize=32,vb=1):
-        self.history=self.model.fit(X,y,epochs=epoch,batch_size=batchsize,verbose=vb)
+
+    def fit(self, X, y, epoch=400, batchsize=32, vb=1):
+        self.history = self.model.fit(X, y, epochs=epoch, batch_size=batchsize, verbose=vb)
         self.plot()
-    
-    def predict(self,X):
+
+    def predict(self, X):
         return self.model.predict_classes(X)
 
     def plot(self):
         plt.plot(self.history.history['accuracy'])
         plt.show()
-
-
-# In[11]:
-
-
-21def evaluate(model_fn,process_fn=None):
-    train_f1=[]
-    test_f1=[]
-    for train_idx,test_idx in kf.split(df):
-        train_df=df.iloc[train_idx].copy()
-        test_df=df.iloc[test_idx].copy()
-
-
-        X_train=process_fn(train_df)
-        y_train=train_df['activity']
-
-        X_test=process_fn(test_df)
-        y_test=test_df['activity']
-        
-        max_val=X_train.max()
-        X_train=X_train/max_val
-        X_test=X_test/max_val
-        
-        model=model_fn()
-        model.fit(X_train,y_train)
-
-        pred=model.predict(X_train)
-        #print(classification_report(y_train,pred))
-        train_f1.append(f1_score(y_train,pred,average='weighted'))
-
-        pred=model.predict(X_test)
-        #print(classification_report(y_test,pred))
-        test_f1.append(f1_score(y_test,pred,average='weighted'))
-
-    plt.boxplot(x=[train_f1,test_f1],positions=[0,1])
-    plt.show()
-
-
-# In[41]:
-
-
-extrat_2dcnn_feat=lambda df:np.array(df[features].values.tolist())[:,:,:,:9].transpose(0,2,3,1)
-cnn2d_model_fn=lambda : My2dCNN()
-evaluate(cnn2d_model_fn,extrat_2dcnn_feat)
-
-
-# In[12]:
 
 
 class My2dCNN_LSTM:
     def __init__(self):
-        self.history=None
-        self.model=self.build_network()
+        self.history = None
+        self.model = self.build_network()
         self.model.summary()
-    
+
     def build_network(self):
-        cnn=tf.keras.Sequential([
-            tf.keras.layers.Conv2D(64, (8,9), (3,1), activation='relu', padding="valid", input_shape=(6,128,9,1)),
-            tf.keras.layers.Conv2D(96, (5,1), (3,1), activation='relu', padding="valid"),
-            tf.keras.layers.Conv2D(128, (5,1), (2,1), activation='relu', padding="valid"),
-            tf.keras.layers.Reshape((6,-1,128)),
-            tf.keras.layers.Lambda(lambda a:tf.math.reduce_mean(a,axis=2),name='Avg'),
+        cnn = tf.keras.Sequential([
+            tf.keras.layers.Conv2D(64, (8, 9), (3, 1), activation='relu', padding="valid", input_shape=(6, 128, 9, 1)),
+            tf.keras.layers.Conv2D(96, (5, 1), (3, 1), activation='relu', padding="valid"),
+            tf.keras.layers.Conv2D(128, (5, 1), (2, 1), activation='relu', padding="valid"),
+            tf.keras.layers.Reshape((6, -1, 128)),
+            tf.keras.layers.Lambda(lambda a: tf.math.reduce_mean(a, axis=2), name='Avg'),
             tf.keras.layers.LSTM(64),
-            tf.keras.layers.Dense(6,activation='softmax')
-        ],name='2dCNN')
-        cnn.compile(loss='sparse_categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+            tf.keras.layers.Dense(6, activation='softmax')
+        ], name='2dCNN')
+        cnn.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
         return cnn
-    
-    def fit(self,X,y,epoch=400,batchsize=32,vb=1):
-        self.history=self.model.fit(X,y,epochs=epoch,batch_size=batchsize,verbose=vb)
+
+    def fit(self, X, y, epoch=400, batchsize=32, vb=1):
+        self.history = self.model.fit(X, y, epochs=epoch, batch_size=batchsize, verbose=vb)
         self.plot()
-    
-    def predict(self,X):
+
+    def predict(self, X):
         return self.model.predict_classes(X)
 
     def plot(self):
         plt.plot(self.history.history['accuracy'])
         plt.show()
+
+
+def evaluate(model_fn, process_fn=None):
+    train_f1 = []
+    test_f1 = []
+    for train_idx, test_idx in kf.split(df):
+        train_df = df.iloc[train_idx].copy()
+        test_df = df.iloc[test_idx].copy()
+
+        X_train = process_fn(train_df)
+        y_train = train_df['activity']
+
+        X_test = process_fn(test_df)
+        y_test = test_df['activity']
+
+        scaler = MinMaxScaler()
+        X_train = scaler.fit_transform(X_train)
+        X_test = scaler.transform(X_test)
+
+        model = model_fn()
+        model.fit(X_train, y_train)
+
+        pred = model.predict(X_train)
+        # print(classification_report(y_train,pred))
+        train_f1.append(f1_score(y_train, pred, average='weighted'))
+
+        pred = model.predict(X_test)
+        # print(classification_report(y_test,pred))
+        test_f1.append(f1_score(y_test, pred, average='weighted'))
+
+    plt.boxplot(x=[train_f1, test_f1], positions=[0, 1])
+    plt.show()
+
+
+# 2d CNN
+class My2dCNN_ch:
+    def __init__(self):
+        self.history = None
+        self.model = self.build_network()
+        self.model.summary()
+
+    def build_network(self):
+        cnn = tf.keras.Sequential([
+            tf.keras.layers.Conv2D(64, (8, 9), (3, 1), activation='relu', padding="valid", input_shape=(128, 9, 6)),
+            tf.keras.layers.Conv2D(96, (5, 1), (3, 1), activation='relu', padding="valid"),
+            tf.keras.layers.Conv2D(128, (5, 1), (2, 1), activation='relu', padding="valid"),
+            tf.keras.layers.GlobalAveragePooling2D(),
+            tf.keras.layers.Dense(64, 'relu'),
+            tf.keras.layers.Dense(6, activation='softmax')
+        ], name='2dCNN')
+        cnn.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        return cnn
+
+    def fit(self, X, y, epoch=400, batchsize=32, vb=1):
+        self.history = self.model.fit(X, y, epochs=epoch, batch_size=batchsize, verbose=vb)
+        self.plot()
+
+    def predict(self, X):
+        return self.model.predict_classes(X)
+
+    def plot(self):
+        plt.plot(self.history.history['accuracy'])
+        plt.show()
+
+
+df = load_dataset()
+class_map = {
+    'looking forward': 0,
+    'looking right': 1,
+    'looking left': 2,
+    'yawning': 3,
+    'looking up': 4,
+    'Talking': 5}
+
+df['activity'] = df.activity.map(class_map)
+
+features = ['doppz']
+label = 'activity'
+kf = KFold(n_splits=3, shuffle=True, random_state=42)
+extract_flat_feat = lambda ddf: np.array(ddf[features].values.tolist())[:, :, :, :9].reshape(-1, 128 * 9)
+rf_model_fn = lambda: RandomForestClassifier(n_estimators=30, criterion='gini', max_depth=20)
+evaluate(rf_model_fn, extract_flat_feat)
+
+extrat_2dcnn_feat = lambda df: np.array(df[features].values.tolist())[:, :, :, :9].transpose(0, 2, 3, 1)
+cnn2d_model_fn = lambda: My2dCNN()
+evaluate(cnn2d_model_fn, extrat_2dcnn_feat)
 
 
 # In[13]:
 
 
-def extrat_2dcnn_lstm_feat(df,window=6):
-    temp=np.array(df[features].values.tolist())[:,:,:,:9].transpose(0,2,3,1)
-    X=np.array([temp[i:i+window] for i in range(temp.shape[0]-window)])
-    y=df[label][window:].values
-    return X,y
+def extrat_2dcnn_lstm_feat(df, window=6):
+    temp = np.array(df[features].values.tolist())[:, :, :, :9].transpose(0, 2, 3, 1)
+    X = np.array([temp[i:i + window] for i in range(temp.shape[0] - window)])
+    y = df[label][window:].values
+    return X, y
 
-cnn_lstm_model_fn=lambda : My2dCNN_LSTM()
+
+cnn_lstm_model_fn = lambda: My2dCNN_LSTM()
 
 
 # In[15]:
 
 
-def evaluate(model_fn,window=6):
-    train_f1=[]
-    test_f1=[]
-    data,label=extrat_2dcnn_lstm_feat(df,window)
-    for train_idx,test_idx in kf.split(data):
+def evaluate(model_fn, window=6):
+    train_f1 = []
+    test_f1 = []
+    data, label = extrat_2dcnn_lstm_feat(df, window)
+    for train_idx, test_idx in kf.split(data):
+        X_train = data[train_idx]
+        y_train = label[train_idx]
 
-        X_train=data[train_idx]
-        y_train=label[train_idx]
+        X_test = data[test_idx]
+        y_test = label[test_idx]
 
-        X_test=data[test_idx]
-        y_test=label[test_idx]
-        
-        max_val=X_train.max()
-        X_train=X_train/max_val
-        X_test=X_test/max_val
-        
-        model=model_fn()
-        model.fit(X_train,y_train)
+        max_val = X_train.max()
+        X_train = X_train / max_val
+        X_test = X_test / max_val
 
-        pred=model.predict(X_train)
-        #print(classification_report(y_train,pred))
-        train_f1.append(f1_score(y_train,pred,average='weighted'))
+        model = model_fn()
+        model.fit(X_train, y_train)
 
-        pred=model.predict(X_test)
-        #print(classification_report(y_test,pred))
-        test_f1.append(f1_score(y_test,pred,average='weighted'))
+        pred = model.predict(X_train)
+        # print(classification_report(y_train,pred))
+        train_f1.append(f1_score(y_train, pred, average='weighted'))
 
-    plt.boxplot(x=[train_f1,test_f1],positions=[0,1])
+        pred = model.predict(X_test)
+        # print(classification_report(y_test,pred))
+        test_f1.append(f1_score(y_test, pred, average='weighted'))
+
+    plt.boxplot(x=[train_f1, test_f1], positions=[0, 1])
     plt.show()
 
 
-# In[16]:
-
-
-evaluate(cnn_lstm_model_fn,window=6)
-
-
-# In[17]:
-
-
-#########
-#2d CNN
-class My2dCNN_ch:
-    def __init__(self):
-        self.history=None
-        self.model=self.build_network()
-        self.model.summary()
-    
-    def build_network(self):
-        cnn=tf.keras.Sequential([
-            tf.keras.layers.Conv2D(64, (8,9), (3,1), activation='relu', padding="valid", input_shape=(128,9,6)),
-            tf.keras.layers.Conv2D(96, (5,1), (3,1), activation='relu', padding="valid"),
-            tf.keras.layers.Conv2D(128, (5,1), (2,1), activation='relu', padding="valid"),
-            tf.keras.layers.GlobalAveragePooling2D(),
-            tf.keras.layers.Dense(64,'relu'),
-            tf.keras.layers.Dense(6,activation='softmax')
-        ],name='2dCNN')
-        cnn.compile(loss='sparse_categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
-        return cnn
-    
-    def fit(self,X,y,epoch=400,batchsize=32,vb=1):
-        self.history=self.model.fit(X,y,epochs=epoch,batch_size=batchsize,verbose=vb)
-        self.plot()
-    
-    def predict(self,X):
-        return self.model.predict_classes(X)
-
-    def plot(self):
-        plt.plot(self.history.history['accuracy'])
-        plt.show()
-
+evaluate(cnn_lstm_model_fn, window=6)
 
 # In[18]:
 
@@ -323,63 +212,12 @@ My2dCNN_ch()
 # In[19]:
 
 
-def extrat_2dcnn_ch_feat(df,window=6):
-    temp=np.array(df[features[0]].values.tolist())[:,:,:9]
-    X=np.array([temp[i:i+window] for i in range(temp.shape[0]-window)]).transpose(0,2,3,1)
-    y=df[label][window:].values
-    return X,y
-
-cnn_ch_model_fn=lambda : My2dCNN_ch()
+def extrat_2dcnn_ch_feat(df, window=6):
+    temp = np.array(df[features[0]].values.tolist())[:, :, :9]
+    X = np.array([temp[i:i + window] for i in range(temp.shape[0] - window)]).transpose(0, 2, 3, 1)
+    y = df[label][window:].values
+    return X, y
 
 
-# In[20]:
-
-
-def evaluate(model_fn,window=6):
-    train_f1=[]
-    test_f1=[]
-    data,label=extrat_2dcnn_ch_feat(df,window)
-    for train_idx,test_idx in kf.split(data):
-
-        X_train=data[train_idx]
-        y_train=label[train_idx]
-
-        X_test=data[test_idx]
-        y_test=label[test_idx]
-        
-        max_val=X_train.max()
-        X_train=X_train/max_val
-        X_test=X_test/max_val
-        
-        model=model_fn()
-        model.fit(X_train,y_train)
-
-        pred=model.predict(X_train)
-        #print(classification_report(y_train,pred))
-        train_f1.append(f1_score(y_train,pred,average='weighted'))
-
-        pred=model.predict(X_test)
-        #print(classification_report(y_test,pred))
-        test_f1.append(f1_score(y_test,pred,average='weighted'))
-
-    plt.boxplot(x=[train_f1,test_f1],positions=[0,1])
-    plt.show()
-
-
-# In[21]:
-
-
+cnn_ch_model_fn = lambda: My2dCNN_ch()
 evaluate(cnn_ch_model_fn)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
--- denoise.py
+++ denoise.py
@@ -1,33 +1,16 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-get_ipython().run_line_magic('matplotlib', 'notebook')
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
 import ast
 import glob
 import seaborn as sns
 import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
-
-
-# In[2]:
 
 
 def read_data(f):
-    df=pd.read_csv(f)
-    df['user']=f.split("_")[1]
+    df = pd.read_csv(f)
+    df['user'] = f.split("_")[1]
     return df
-
-
-# In[3]:
 
 
 def string_to_matrix(df):
@@ -37,29 +20,24 @@ def string_to_matrix(df):
     return doppz
 
 
-# In[4]:
-
-
 def noise_detector(df):
     doppz = df['doppz'].values
     index_val = []
     for i, v in enumerate(doppz):
         v = np.array(v)
-        sum_val = v.reshape(-1,128*64).sum(axis=1)
+        sum_val = v.reshape(-1, 128 * 64).sum(axis=1)
         if sum_val > 25000000:
             index_val.append(i)
-    
-    fig, axs = plt.subplots(1,2,figsize=(9,7), gridspec_kw={'width_ratios': [8, 1]})
+
+    fig, axs = plt.subplots(1, 2, figsize=(9, 7), gridspec_kw={'width_ratios': [8, 1]})
     fig.show()
     for i in index_val:
-        axs[0].cla();axs[1].cla()
+        axs[0].cla()
+        axs[1].cla()
         axs[0].set_title(df['activity'][i])
-        sns.heatmap(doppz[i],ax=axs[0],cbar_ax=axs[1])
+        sns.heatmap(doppz[i], ax=axs[0], cbar_ax=axs[1])
         fig.canvas.draw()
     return index_val
-
-
-# In[5]:
 
 
 def noise_removal(df):
@@ -69,7 +47,7 @@ def noise_removal(df):
     last_currect = 0
     for i, v in enumerate(doppz):
         v = np.array(v)
-        sum_val = v.reshape(-1,128*64).sum(axis=1)
+        sum_val = v.reshape(-1, 128 * 64).sum(axis=1)
         if sum_val > 25000000:
             doppz[i] = doppz[last_currect]
         else:
@@ -77,75 +55,48 @@ def noise_removal(df):
     return doppz
 
 
-# In[28]:
-
-
 def plot_animation(df):
     doppz = np.array(df['doppz'].values.tolist())
-    fig, axs = plt.subplots(1,2,figsize=(9,7), gridspec_kw={'width_ratios': [8, 1]})
+    fig, axs = plt.subplots(1, 2, figsize=(9, 7), gridspec_kw={'width_ratios': [8, 1]})
     fig.show()
     for i in range(doppz.shape[0]):
-        axs[0].cla();axs[1].cla()
+        axs[0].cla()
+        axs[1].cla()
         axs[0].set_title(df['activity'][i])
-        sns.heatmap(doppz[i],ax=axs[0],cbar_ax=axs[1])
+        sns.heatmap(doppz[i], ax=axs[0], cbar_ax=axs[1])
         fig.canvas.draw()
-        
-def plot_relation(df,xlim0=100,xlim1=700):
+
+
+def plot_relation(df, xlim0=100, xlim1=700):
     doppz = np.array(df['doppz'].values.tolist())
-    fig=plt.figure(figsize=(12,7))
-    ax=fig.add_subplot(311)
-    map_dict=dict(zip(['looking forward', 'Talking', 'yawning', 'looking right', 'looking left', 'looking up'],
-                      ['k', 'orange', 'blue', 'cyan', 'red','green']))
-    labels=df['activity'].map(map_dict)
+    fig = plt.figure(figsize=(12, 7))
+    ax = fig.add_subplot(311)
+    map_dict = dict(zip(['looking forward', 'Talking', 'yawning', 'looking right', 'looking left', 'looking up'],
+                        ['k', 'orange', 'blue', 'cyan', 'red', 'green']))
+    labels = df['activity'].map(map_dict)
     print(labels)
-    sum_val = doppz.reshape(-1,128*64).sum(axis=1)
-    ax.scatter(range(len(sum_val)),sum_val,c=labels)
-    ax.plot(range(len(sum_val)),len(sum_val),linestyle='--',c='k')
-    ax.set_xlim(xlim0,xlim1)
+    sum_val = doppz.reshape(-1, 128 * 64).sum(axis=1)
+    ax.scatter(range(len(sum_val)), sum_val, c=labels)
+    ax.plot(range(len(sum_val)), len(sum_val), linestyle='--', c='k')
+    ax.set_xlim(xlim0, xlim1)
 
-    pathes = [mpatches.Patch(color=c, label=v) for v,c in map_dict.items()]
-    ax.legend(handles=pathes,ncol=3,bbox_to_anchor=(0.3, 1.05))
+    pathes = [mpatches.Patch(color=c, label=v) for v, c in map_dict.items()]
+    ax.legend(handles=pathes, ncol=3, bbox_to_anchor=(0.3, 1.05))
 
-    ax=fig.add_subplot(312)
+    ax = fig.add_subplot(312)
     ax.plot(df['mar'].values, label='mar')
-    ax.set_xlim(xlim0,xlim1)
+    ax.set_xlim(xlim0, xlim1)
     ax.legend()
 
-    ax=fig.add_subplot(313)
+    ax = fig.add_subplot(313)
     ax.plot(df['y'].values, label='y')
     ax.plot(df['x'].values, label='x')
     ax.legend()
-    ax.set_xlim(xlim0,xlim1)
-
-
-# In[29]:
+    ax.set_xlim(xlim0, xlim1)
 
 
 df = pd.concat([read_data(f) for f in glob.glob("./data/*")]).reset_index(drop=False)
 df['doppz'] = string_to_matrix(df['doppz'])
 df['doppz'] = noise_removal(df).tolist()
-
-
-# In[30]:
-
-
 # plot_animation(df)
-
-
-# In[31]:
-
-
-plot_relation(df[df.user=='bishakh1'],0,len(df[df.user=='bishakh1']))
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+plot_relation(df[df.user == 'bishakh1'], 0, len(df[df.user == 'bishakh1']))
