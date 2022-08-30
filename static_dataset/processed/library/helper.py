3f5b5799edc1104e5ef9ec86e82e444fcb6a105f
arghasen10 <arghasen10@gmail.com>
2022-08-30 23:43:36 +0530

    updated ignore

---
+++ static_dataset/processed/library/helper.py
@@ -0,0 +1,13 @@
+import glob
+import numpy as np
+import pandas as pd
+
+def read_data(f):
+    df=pd.read_csv(f)
+    df['user']=f.split("_")[1]
+    return df
+
+def load_dataset(loc="./data"):
+    df=pd.concat([read_data(f) for f in glob.glob(loc+"/*")])
+    df['doppz']=df['doppz'].apply(lambda e:eval(e))
+    return df[['user','doppz','activity']]
