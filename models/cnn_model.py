e10c07e57f22d5ececa9c28e58fef78544e28d15
arghasen10 <arghasen10@gmail.com>
2022-08-31 23:35:38 +0530

    models

---
+++ models/cnn_model.py
@@ -0,0 +1,58 @@
+import tensorflow as tf
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.applications.vgg16 import preprocess_input
+from tensorflow.keras import layers, models
+import sys
+from library.augumentation import Dataset
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+sys.path.append('../')
+
+
+data = Dataset(loc='../static_dataset/processed/denoised/*',
+               class_count=1000,
+               frame_stack=4,
+               dop_min=1375,
+               dop_max=5293, )
+
+lbl_map = \
+    {'looking forward': 0,
+     'Talking': 1,
+     'yawning': 2,
+     'looking right': 3,
+     'looking left': 4,
+     'looking up': 5}
+
+# X=preprocess_input(np.uint8(data.data*255))
+X_norm = data.data
+y = to_categorical(np.array(list(map(lambda e: lbl_map[e], data.label))), num_classes=6)
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(32, (5, 5), (1, 1), padding="same", activation='relu', input_shape=(48, 48, 4)),
+    tf.keras.layers.MaxPool2D((3, 3)),
+    tf.keras.layers.Conv2D(64, (3, 3), (1, 1), padding="same", activation='relu'),
+    tf.keras.layers.MaxPool2D((2, 2)),
+    tf.keras.layers.Conv2D(96, (3, 3), (1, 1), padding="same", activation='relu'),
+    tf.keras.layers.MaxPool2D((2, 2)),
+    tf.keras.layers.GlobalAveragePooling2D(),
+    tf.keras.layers.Dense(32, "relu"),
+    tf.keras.layers.Dense(6, "softmax")
+])
+
+model.compile(loss="categorical_crossentropy", optimizer="adam", metrics="accuracy")
+
+model.summary()
+
+X_train, X_test, y_train, y_test = train_test_split(X_norm, y, test_size=0.3, random_state=42)
+model.fit(
+    X_train,
+    y_train,
+    epochs=100,
+    validation_split=0.2,
+    batch_size=32)
+
+model.evaluate(X_test, y_test)
---
+++ models/rf_model.py
@@ -0,0 +1,67 @@
+import sys
+from library.augumentation import Dataset
+import numpy as np
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+
+sys.path.append('../')
+
+
+def PoolOp(img, ksize=(16, 16), op=np.mean):
+    arr = []
+    i_h, i_w, i_c = img.shape
+    k_h, k_w = ksize
+    row = []
+    for c in range(i_c):
+        for i in range(i_h // k_h):
+            for j in range(i_w // k_w):
+                row.append(op(img[k_h * i:k_h * i + k_h, k_w * j:k_w * j + k_w, c]))
+    return np.array(row)
+
+
+preprocess = lambda v: np.concatenate([PoolOp(v, op=np.min),
+                                       PoolOp(v, op=np.max),
+                                       PoolOp(v, op=np.mean),
+                                       PoolOp(v, op=np.std)]).flatten().tolist()
+data = Dataset(loc='../static_dataset/processed/denoised/*',
+               class_count=1000,
+               frame_stack=2,
+               dop_min=1375,
+               dop_max=5293, )
+
+lbl_map = \
+    {'looking forward': 0,
+     'Talking': 1,
+     'yawning': 2,
+     'looking right': 3,
+     'looking left': 4,
+     'looking up': 5}
+
+label = np.array(list(map(lambda e: lbl_map[e], data.label)))
+
+X = np.array([preprocess(d) for d in data.data])
+X_train, X_test, y_train, y_test = train_test_split(X, label, test_size=0.3, random_state=42)
+rf = RandomForestClassifier()
+rf.fit(X_train, y_train)
+pred = rf.predict(X_train)
+
+print(classification_report(y_train, pred))
+pred = rf.predict(X_test)
+
+print(classification_report(y_test, pred))
+
+sns.heatmap(confusion_matrix(y_test, pred), annot=True)
+plt.xticks(ticks=list(lbl_map.values()), labels=list(lbl_map.keys()), rotation=45)
+plt.yticks(ticks=list(lbl_map.values()), labels=list(lbl_map.keys()), rotation=45)
+plt.tight_layout()
