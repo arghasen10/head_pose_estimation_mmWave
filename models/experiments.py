9a98f02bd8ebe51a2918ea8b03106fb26336adfb
arghasen10 <arghasen10@gmail.com>
2022-09-07 16:36:08 +0530

    saved paths

--- models/experiments.py
+++ models/experiments.py
@@ -2,31 +2,31 @@ import datetime
 from models import read_data,Model,rf_model,cnn_model,vgg16_model
 
 runtime=datetime.datetime.now().strftime("%Y_%m_%d-%I:%M:%S_%p")
-
+saved_paths=['{}','./saved_weights/'+runtime+'_vgg16_{}.h5','./saved_weights/'+runtime+'_cnn_{}.h5']
 #exp1-merged
 train_pattern="../*_dataset/processed/denoised/*"
 X_train, X_test, y_train, y_test=read_data(train_pattern=train_pattern,class_count=1000)
 #rf+vgg16+cnn_model
-for md_class in [rf_model,vgg16_model,cnn_model]:
+for md_class,sp in zip([rf_model,vgg16_model,cnn_model],saved_paths):
     model=Model(X_train, X_test, y_train, y_test,md_class)
-    model.train()
+    model.train(sp.format('merged'))
     model.test('merged'+runtime)
 
 #exp2-static
 train_pattern="../static_dataset/processed/denoised/*"
 X_train, X_test, y_train, y_test=read_data(train_pattern=train_pattern,class_count=600)
 #rf+vgg16+cnn_model
-for md_class in [rf_model,vgg16_model,cnn_model]:
+for md_class,sp in zip([rf_model,vgg16_model,cnn_model],saved_paths):
     model=Model(X_train, X_test, y_train, y_test,md_class)
-    model.train()
+    model.train(sp.format('static'))
     model.test('static'+runtime)
 
 #exp3-driving
 train_pattern="../driving_dataset/processed/denoised/*"
 X_train, X_test, y_train, y_test=read_data(train_pattern=train_pattern,class_count=600)
 #rf+vgg16+cnn_model
-for md_class in [rf_model,vgg16_model,cnn_model]:
+for md_class,sp in zip([rf_model,vgg16_model,cnn_model],saved_paths):
     model=Model(X_train, X_test, y_train, y_test,md_class)
-    model.train()
+    model.train(sp.format('driving'))
     model.test('driving'+runtime)
 
