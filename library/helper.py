0e7addc51ea3b8294d3a05ab453899d2b2937d37
arghasen10 <arghasen10@gmail.com>
2022-09-07 17:59:31 +0530

    all exps

--- library/helper.py
+++ library/helper.py
@@ -7,7 +7,18 @@ def read_data(f):
     df['user']=f.split("_")[1]
     return df
 
-def load_dataset(loc="./data/*"):
-    df=pd.concat([read_data(f) for f in glob.glob(loc)])
+def modified_glob(pattern, exclude_substr):
+    files=[f for f in glob.glob(pattern) if exclude_substr not in f]
+    print(files)
+    return files
+
+def load_dataset(loc="./data/*!a"):
+    temp=loc.split("!")
+    if len(temp)==1:
+        pattern=temp[0]
+        exclude_substr='@#'
+    else:
+        pattern,exclude_substr=temp
+    df=pd.concat([read_data(f) for f in modified_glob(pattern,exclude_substr)])
     df['doppz']=df['doppz'].apply(lambda e:eval(e))
     return df[['user','doppz','activity']].reset_index(drop=True)
--- models/experiments.py
+++ models/experiments.py
@@ -30,3 +30,50 @@ for md_class,sp in zip([rf_model,vgg16_model,cnn_model],saved_paths):
     model.train(sp.format('driving'))
     model.test('driving'+runtime)
 
+#exp4-leave-one-out
+#static
+for user in ['argha','anirban','bishakh','aritra']:
+    train_pattern=f"../static_dataset/processed/denoised/*!{user}"
+    test_pattern=f"../static_dataset/processed/denoised/*_{user}_*"
+
+    X_train, X_test, y_train, y_test=read_mydata(train_pattern=train_pattern,test_pattern=test_pattern,class_count=200)
+    #rf+vgg16+cnn_model
+    for md_class,sp in zip([rf_model,vgg16_model,cnn_model],saved_paths):
+        model=Model(X_train, X_test, y_train, y_test,md_class)
+        model.train(sp.format(f'static_{user}_loO'))
+        model.test(f'static_{user}_loO'+runtime)
+
+#Driving
+for user in ['bishakh3', 'anirban1', 'bishakh2', 'sugandh3', 'anirban2', 'sugandh2', 'bishakh1', 'sugandh1']:
+    train_pattern=f"../driving_dataset/processed/denoised/*!{user}"
+    test_pattern=f"../driving_dataset/processed/denoised/*_{user}_*"
+
+    X_train, X_test, y_train, y_test=read_mydata(train_pattern=train_pattern,test_pattern=test_pattern,class_count=200)
+    #rf+vgg16+cnn_model
+    for md_class,sp in zip([rf_model,vgg16_model,cnn_model],saved_paths):
+        model=Model(X_train, X_test, y_train, y_test,md_class)
+        model.train(sp.format(f'driving_{user}_loO'))
+        model.test(f'driving_{user}_loO'+runtime)
+
+#exp5-personalized
+#static
+for user in ['argha','anirban','bishakh','aritra']:
+    train_pattern=f"../static_dataset/processed/denoised/*_{user}_*"
+
+    X_train, X_test, y_train, y_test=read_mydata(train_pattern=train_pattern,class_count=200)
+    #rf+vgg16+cnn_model
+    for md_class,sp in zip([rf_model,vgg16_model,cnn_model],saved_paths):
+        model=Model(X_train, X_test, y_train, y_test,md_class)
+        model.train(sp.format(f'static_{user}_per'))
+        model.test(f'static_{user}_per'+runtime)
+
+#Driving
+for user in ['bishakh3', 'anirban1', 'bishakh2', 'sugandh3', 'anirban2', 'sugandh2', 'bishakh1', 'sugandh1']:
+    train_pattern=f"../driving_dataset/processed/denoised/*_{user}_*"
+
+    X_train, X_test, y_train, y_test=read_mydata(train_pattern=train_pattern,class_count=200)
+    #rf+vgg16+cnn_model
+    for md_class,sp in zip([rf_model,vgg16_model,cnn_model],saved_paths):
+        model=Model(X_train, X_test, y_train, y_test,md_class)
+        model.train(sp.format(f'driving_{user}_per'))
+        model.test(f'driving_{user}_per'+runtime)
